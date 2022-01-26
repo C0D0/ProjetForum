ef9aadb58fa43c0cbcb104891761472ae33863f9
PLucas0509 <89928055+PLucas0509@users.noreply.github.com>
2022-01-26 11:21:33 +0100

    css

--- my-questions.php
+++ my-questions.php
@@ -26,9 +26,11 @@
                         <p class="card-text">
                             <?= $question['description']; ?>
                         </p>
-                        <a href="article.php?id=<?= $question['id']; ?>" class="btn btn-primary">Accéder à la question</a>
-                        <a href="edit-question.php?id=<?= $question['id']; ?>" class="btn btn-warning">Modifier la question</a>
-                        <a href="actions/questions/deleteQuestionAction.php?id=<?= $question['id']; ?>" class="btn btn-danger">Supprimer la question</a>
+                        <br>
+                        <br>
+                        <a href="article.php?id=<?= $question['id']; ?>" class="smBox1">Accéder à la question</a>
+                        <a href="edit-question.php?id=<?= $question['id']; ?>" class="smBox1">Modifier la question</a>
+                        <a href="actions/questions/deleteQuestionAction.php?id=<?= $question['id']; ?>" class="smBox1">Supprimer la question</a>
                     </div>
                     </div>
                 </div>
